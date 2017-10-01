3b3865879554f0b1474c896da143176d50a43087
Alando Appling <alandoappling@gmail.com>
2017-10-01 14:45:00 -0700

    Added navbar,setup devise auth for user model, setup ckeditor for post model

---
+++ rewindhiphop/app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def tapes
+  end
+
+  def user
+  end
+end
--- rewindhiphop/config/routes.rb
+++ rewindhiphop/config/routes.rb
@@ -1,8 +1,35 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'users/new'
+  
+  mount Ckeditor::Engine => '/ckeditor'
+  resources :posts
+  	
+  get 'home', to: 'static_pages#home', as: 'home'
 
-  root to: 'home#index'
+  get 'tapes', to: 'static_pages#tapes', as: 'tapes'
+
+  get 'user', to: 'static_pages#user', as: 'user'
+  
+  devise_for :users do 
+  
+	  get 'users/sign_in', to: 'devise/sessions#new'
+
+	  get 'users/sign_in', to: 'devise/sessions#create'
+
+	  get 'users/sign_out' => 'devise/sessions#destroy'
+
+	  get 'users/password/new', to: 'devise/passwords#new'
+
+	  get 'users/password/edit', to: 'devise/passwords#edit'
+
+	  get 'users/cancel', to: 'devise/registrations#cancel'
+
+	  get 'users/sign_up', to: 'devise/registrations#new'
+
+	  get 'users/edit', to: 'devise/registrations#edit'
+
+	end
+
+  root to: 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
